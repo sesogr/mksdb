fca08b59b4ebcdd892c15bed976cf7c96dd34550
AnSc <mksdb@sedna-soft.de>
2021-06-15 15:39:44 +0000

    Add tests for KSD-T-3

--- test/real-db.php
+++ test/real-db.php
@@ -13,3 +13,5 @@ $db = new PDO(
 it('yields one single result for "Wolfgangsee Rößl" (KSD-T-6)', fn() => count(gatherSearchResults('Wolfgangsee Rößl', $db)) === 1);
 it('can search for origin "Himmelstür" (KSD-T-1)', fn() => hasAtLeastSoManyResultsWhichAllMatchCallback($db, 'Himmelstür', 1, fn(SearchResult $r) => mb_stripos($r->origin, 'HIMMELSTÜR') !== false));
 it('can search for origin "Viktoria" (KSD-T-1)', fn() => hasAtLeastSoManyResultsWhichAllMatchCallback($db, 'Viktoria', 1, fn(SearchResult $r) => mb_stripos($r->origin, 'VIKTORIA') !== false));
+it('ignores letter-case for keywords like "vIkToRiA" (KSD-T-3)', fn() => hasAtLeastSoManyResultsWhichAllMatchCallback($db, 'vIkToRiA', 1, fn(SearchResult $r) => mb_strpos($r->origin, 'Viktoria') !== false));
+it('ignores letter-case for phrases like "NoCh EiNmAl DiE hÄnDe" (KSD-T-3)', fn() => hasAtLeastSoManyResultsWhichAllMatchCallback($db, '"NoCh EiNmAl DiE hÄnDe"', 1, fn(SearchResult $r) => mb_strpos($r->title, 'Hände') !== false));
