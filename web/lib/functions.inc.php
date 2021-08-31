07536d4f0431567826e827b6df261bd7151f9381
AnSc <mksdb@sedna-soft.de>
2021-08-31 16:01:27 +0000

    Merge config functions into functions.inc.php

--- web/lib/functions.inc.php
+++ web/lib/functions.inc.php
@@ -1,5 +1,9 @@
 <?php declare(strict_types=1);
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
+require_once __DIR__ . '/types.inc.php';
 function buildQuery(PDO $db): PDOStatement
 {
     $songColumns = [
@@ -101,12 +105,56 @@ function gatherSearchResults(string $search, PDO $db): array
         : [];
 }
 
+function handleCustomRequest(string $operation, string $tableName, ServerRequestInterface $request, $environment)
+{
+    if ($operation === 'list' && $tableName === 'search') {
+        $environment->search = $request->getQueryParams();
+    }
+}
+
+function handleCustomResponse(string $operation, string $tableName, ResponseInterface $response, $environment)
+{
+    if (isset($environment->search['q'])) {
+        $factory = new Psr17Factory();
+        $config = include __DIR__ . '/../config.inc.php';
+        $db = new PDO(
+            sprintf(
+                "mysql:host=%s;port=%d;dbname=%s;charset=%s",
+                $config['address'],
+                $config['port'],
+                $config['database'],
+                'utf8mb4'
+            ),
+            $config['username'],
+            $config['password']
+        );
+        $content = json_encode(
+            ['records' => array_map(
+                function ($x) {
+                    $x->id = intval($x->id);
+                    return $x;
+                },
+                gatherSearchResults($environment->search['q'], $db)
+            )],
+            JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+        );
+        $stream = $factory->createStream($content);
+        $stream->rewind();
+        return $factory->createResponse(200)
+            ->withHeader('Content-Type', 'application/json; charset=UTF-8')
+            ->withHeader('Content-Length', strlen($content))
+            ->withBody($stream);
+    }
+    return $response;
+}
+
 /**
  * @param string $search
  *
  * @return array [keywords, ranges, excludedKeywords, excludedRanges]
  */
-function parseSearch(string $search): array {
+function parseSearch(string $search): array
+{
     $wildcardSearch = str_replace('*', '%', str_replace('%', '%%', $search));
     [$nonPhrases, $phrases, $excludedPhrases] = splitPhrases($wildcardSearch);
     [$nonRanges, $ranges, $excludedRanges] = splitRanges($nonPhrases);
@@ -117,6 +165,11 @@ function parseSearch(string $search): array {
         array_merge($excludedKeywords, $excludedPhrases),
         $excludedRanges,
     ];
+}
+
+function preventMutationOperations(string $operation, string $tableName)
+{
+    return $operation === 'list' || $operation === 'read';
 }
 
 /**
