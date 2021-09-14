d81010354aeffad9950fd3e3e39da9ff8d9aa94d
MFH <maximilian-felix.helm@stud.hszg.de>
2021-09-14 17:12:44 +0200

    updating tests
    - adding test for v1 search
    - adding test for excluded keywords

--- web/lib/test-functions.inc.php
+++ web/lib/test-functions.inc.php
@@ -1,9 +1,22 @@
 <?php declare(strict_types=1);
 
-function hasAtLeastSoManyResultsWhichAllMatchCallback(PDO $db, string $search, int $minResults, callable $matcher): bool
+function hasAtLeastSoManyResultsWhichAllMatchCallbackV2(PDO $db, string $search, int $minResults, callable $matcher): bool
 {
     $stats = array_reduce(
         gatherSearchResults($search, $db),
+        fn($accu, SearchResult $item) => [
+            'total' => 1 + $accu['total'],
+            'match' => ($matcher($item) ? 1 : 0) + $accu['match'],
+        ],
+        ['total' => 0, 'match' => 0]
+    );
+    return $stats['total'] >= $minResults && $stats['match'] === $stats['total'];
+}
+
+function hasAtLeastSoManyResultsWhichAllMatchCallbackV1(PDO $db, string $search, int $minResults, callable $matcher): bool
+{
+    $stats = array_reduce(
+        gatherSearchResultsWithShortcuts($search, $db),
         fn($accu, SearchResult $item) => [
             'total' => 1 + $accu['total'],
             'match' => ($matcher($item) ? 1 : 0) + $accu['match'],
