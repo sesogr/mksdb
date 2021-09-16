59983417aef4ffdf289cf62d44cf20703106ee27
MFH <maximilian-felix.helm@stud.hszg.de>
2021-09-16 11:04:37 +0200

    add new helper functions for test (for unordered array deep compare)

--- web/lib/test-functions.inc.php
+++ web/lib/test-functions.inc.php
@@ -29,3 +29,58 @@ function hasAtLeastSoManyResultsWhichAllMatchCallbackV1(PDO $db, string $search,
     );
     return $stats['total'] >= $minResults && $stats['match'] === $stats['total'];
 }
+
+/**
+ * test if the array is associative
+ * @param array $arr
+ * @return bool true if arr is associative, false if it is numerical indexed (without holes in keys)
+ */
+function isArrayAssociative(array $arr): bool {
+    if (array() === $arr) return false;
+    return array_keys($arr) !== range(0, count($arr) - 1);
+}
+
+/**
+ * checks if the two arrays have equal contents (same keys and values; without checking order of values)
+ * @param array $a
+ * @param array $b
+ * @return bool
+ */
+function compareArrayDeep(array $a, array $b): bool {
+    if(count($a) !== count($b)) return false;// if length is the same, we can compare by iterating ver only one array-key-set
+
+    foreach($a as $kA => $vA){
+        if(!isset($b[$kA]))
+            return false;
+
+        $vB = $b[$kA];
+
+        if(gettype($vA) !== gettype($vB))
+            return false;
+
+        if(gettype($vA) === 'array'){// recursively compare arrays
+            if(isArrayAssociative($vA)) {
+                if (!isArrayAssociative($vB)) return false;
+
+                if (!compareArrayDeep($vA, $vB))
+                    return false;
+            } else {
+                if (isArrayAssociative($vB)) return false;
+
+                // sort number-indexed arrays so that order does not matter
+                $vACpy = $vA;
+                asort($vACpy);
+                $vBCpy = $vACpy;
+                asort($vBCpy);
+
+                if (!compareArrayDeep($vACpy, $vBCpy))
+                    return false;
+            }
+        }else{// just use standard value comparison
+            if($vA !== $vB)
+                return false;
+        }
+    }
+
+    return true;
+}
